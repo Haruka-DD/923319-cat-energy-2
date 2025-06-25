8d8a3e6148e45099d5234004029c1349a05d74a3
Haruka-DD <haruka1917@gmail.com>
2025-06-26 00:01:33 +0500

    Испраляет отступы по ПП

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
-/* в этот файл добавляет скрипты*/
+document.querySelector('.main-nav').classList.remove('main-nav--nojs');
+
+const menuToggle = document.querySelector('.main-nav__toggle');
+const navigationList = document.querySelector('.main-nav__list');
+
+menuToggle.addEventListener ('click', () => {
+  navigationList.classList.toggle('main-nav__list--opened');
+  menuToggle.classList.toggle('main-nav__toggle--active');
+});
