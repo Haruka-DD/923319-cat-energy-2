281675be1af4e9e078f99825ca231cc24221a3f1
Haruka-DD <haruka1917@gmail.com>
2025-06-29 17:12:57 +0500

    Корректирует по ПП

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,5 @@
 document.querySelector('.main-nav').classList.remove('main-nav--nojs');
+document.querySelector('.contacts__map-wrapper').classList.remove('contacts__map-wrapper--nojs');
 
 const menuToggle = document.querySelector('.main-nav__toggle');
 const navigationList = document.querySelector('.main-nav__list');
